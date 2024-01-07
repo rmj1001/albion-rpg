a9bc0512dd01596dad8b3d285640d3a49f8d3560
Genesius of Walsingham <rmichael1001@gmail.com>
2024-01-07 10:51:52 -0500

    adding panic option to developer menu

--- src/menus/game/profile/d1_developer_menu.rs
+++ src/menus/game/profile/d1_developer_menu.rs
@@ -1,4 +1,23 @@
-use albion_termrpg::lib::user_profile::UserProfile;
+use albion_termrpg::lib::{
+    tui::{self, page_header},
+    user_profile::UserProfile,
+};
 
-#[allow(unused_variables)]
-pub fn main(user: &mut UserProfile) {}
+pub fn main(user: &mut UserProfile) {
+    page_header(
+        "Developer Settings",
+        Some("Use ↑ ↓ keys to select an option below, then press ENTER/RETURN to run it"),
+    );
+
+    let choice = tui::dialogue::selector(
+        &["1. Throw a panic", "NAV: Go Back to Main Menu"],
+        0,
+        Some(""),
+    );
+
+    match choice {
+        0 => panic!("This is a panic!"),
+        1 => crate::menus::game::main::menu(user),
+        _ => panic!("Dialogue picked option out of bounds"),
+    }
+}
