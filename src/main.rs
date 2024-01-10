26e8e0505d2a428bf2ca3ffbf58180fa4ea604bd
Genesius of Walsingham <rmichael1001@gmail.com>
2024-01-09 19:07:52 -0500

    renamed project to albion_rpg

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,4 @@
-pub mod menus {
-    pub mod accounts {
-        pub mod login;
-        pub mod main;
-        pub mod register;
-    }
-
-    pub mod game {
-        pub mod combat {
-            pub mod c1_the_stronghold;
-            pub mod c2_wander_realm;
-        }
-
-        pub mod economy {
-            pub mod e1_the_guilds;
-            pub mod e2_the_bank;
-            pub mod e3_trading_post;
-            pub mod e4_weapons_shop;
-            pub mod e5_armor_shop;
-            pub mod e6_mystic_shop;
-            pub mod e7_celestial_shop;
-        }
-
-        pub mod profile {
-            pub mod d1_developer_menu;
-            pub mod n1_settings;
-            pub mod p1_inventory;
-            pub mod p2_hall_of_records;
-        }
-
-        pub mod main;
-    }
-}
-
 fn main() {
-    albion_termrpg::lib::os::deter_unsupported_os();
-    menus::accounts::main::menu();
+    albion_rpg::lib::os::deter_unsupported_os();
+    albion_rpg::menus::accounts::main::menu();
 }
