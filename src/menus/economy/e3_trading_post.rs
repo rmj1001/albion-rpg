6be10a96950dcf9983ae597111e387919738728f
Genesius of Walsingham <rmichael1001@gmail.com>
2024-01-17 12:05:08 -0500

    added skeletons for initial shop menu and selecting items

--- src/menus/economy/e3_trading_post.rs
+++ src/menus/economy/e3_trading_post.rs
@@ -1,15 +1,78 @@
 use crate::{
-    lib::tui::{page_header, press_enter_to_continue},
+    lib::{
+        input::{
+            input_generic, invalid_input, out_of_bounds, prompt_input_completion,
+            select_from_str_array,
+        },
+        tui::{page_header, press_enter_to_continue},
+    },
     user::profile::UserProfile,
 };
 
-// TODO: Trading Post
 pub fn main(user: &mut UserProfile) {
     page_header("Trading Post", crate::lib::tui::HeaderSubtext::None);
 
     user.inventory.print_table();
 
-    press_enter_to_continue();
+    let buysell = select_from_str_array(&["1. Purchase", "2. Sell", "NAV: Go Back"], None);
+
+    match buysell {
+        0 => purchase(user),
+        1 => sell(user),
+        2 => crate::menus::game_menu::main(user),
+        _ => out_of_bounds(None),
+    }
 
     crate::menus::game_menu::main(user);
+}
+
+pub fn purchase(user: &mut UserProfile) {
+    let (item, quantity) = get_item_and_quantity(user);
+
+    // TODO: inventory arithmetic here.
+}
+
+pub fn sell(user: &mut UserProfile) {
+    let (item, quantity) = get_item_and_quantity(user);
+
+    // TODO: inventory arithmetic here.
+}
+
+fn get_item_and_quantity(user: &mut UserProfile) -> (String, usize) {
+    let items: Vec<String> = vec![
+        "bait".to_string(),
+        "seeds".to_string(),
+        "furs".to_string(),
+        "fish".to_string(),
+        "food".to_string(),
+        "wood".to_string(),
+        "ore".to_string(),
+        "ingots".to_string(),
+        "potions".to_string(),
+        "rubies".to_string(),
+        "magic scrolls".to_string(),
+        "bones".to_string(),
+        "dragon hides".to_string(),
+        "runic_tablets".to_string(),
+    ];
+
+    let item = prompt_input_completion(
+        "Type the name of the item you wish to purchase.",
+        items.clone(),
+    );
+
+    if !items.contains(&item.to_lowercase()) {
+        invalid_input(Some(&item), None, true);
+        main(user);
+    }
+
+    let quantity: Result<usize, &str> = input_generic::<usize>("Quantity");
+
+    if quantity.is_err() {
+        eprintln!("{}", quantity.unwrap());
+        press_enter_to_continue();
+        main(user);
+    }
+
+    (item, quantity.unwrap())
 }
--- src/menus/economy/e4_weapons_shop.rs
+++ src/menus/economy/e4_weapons_shop.rs
@@ -1,5 +1,64 @@
-use crate::user::profile::UserProfile;
+use crate::{
+    lib::{
+        input::{
+            input_generic, invalid_input, out_of_bounds, prompt_input_completion,
+            select_from_str_array,
+        },
+        tui::{page_header, press_enter_to_continue},
+    },
+    user::profile::UserProfile,
+};
 
-// TODO: Weapons Shop
-#[allow(unused_variables)]
-pub fn main(user: &mut UserProfile) {}
+pub fn main(user: &mut UserProfile) {
+    page_header("Weapons Shop", crate::lib::tui::HeaderSubtext::None);
+
+    user.weapons.print_table();
+
+    let buysell = select_from_str_array(&["1. Purchase", "2. Sell", "NAV: Go Back"], None);
+
+    match buysell {
+        0 => purchase(user),
+        1 => sell(user),
+        2 => crate::menus::game_menu::main(user),
+        _ => out_of_bounds(None),
+    }
+
+    crate::menus::game_menu::main(user);
+}
+
+pub fn purchase(user: &mut UserProfile) {
+    let (item, quantity) = get_item_and_quantity(user);
+
+    // TODO: inventory arithmetic here.
+}
+
+pub fn sell(user: &mut UserProfile) {
+    let (item, quantity) = get_item_and_quantity(user);
+
+    // TODO: inventory arithmetic here.
+}
+
+fn get_item_and_quantity(user: &mut UserProfile) -> (String, usize) {
+    // TODO: Populate with weapon names
+    let items: Vec<String> = vec![];
+
+    let item = prompt_input_completion(
+        "Type the name of the item you wish to purchase.",
+        items.clone(),
+    );
+
+    if !items.contains(&item.to_lowercase()) {
+        invalid_input(Some(&item), None, true);
+        main(user);
+    }
+
+    let quantity: Result<usize, &str> = input_generic::<usize>("Quantity");
+
+    if quantity.is_err() {
+        eprintln!("{}", quantity.unwrap());
+        press_enter_to_continue();
+        main(user);
+    }
+
+    (item, quantity.unwrap())
+}
