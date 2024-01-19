513ec705ec7a12c846ad968246f4ef65a2033b0b
Genesius of Walsingham <rmichael1001@gmail.com>
2024-01-18 23:53:38 -0500

    added "This is not yet implemented" messages to combat section

--- src/menus/combat/c2_wander_realm.rs
+++ src/menus/combat/c2_wander_realm.rs
@@ -1,5 +1,14 @@
-use crate::user::profile::UserProfile;
+use crate::{
+    lib::tui::{page_header, press_enter_to_continue, HeaderSubtext},
+    user::profile::UserProfile,
+};
 
-// TODO: Wandering the Realm (random encounters)
-#[allow(unused_variables)]
-pub fn main(user: &mut UserProfile) {}
+// TODO: Wandering the Realm
+pub fn main(user: &mut UserProfile) {
+    page_header("Wandering the Realm", HeaderSubtext::None);
+
+    println!("This is not yet implemented.");
+    press_enter_to_continue();
+
+    crate::menus::game_menu::main(user);
+}
