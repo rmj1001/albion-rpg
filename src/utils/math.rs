7436aec8facb92989b4b3e8288ddbab827938887
Roy Conn <rmichael1001@gmail.com>
2024-02-16 11:02:38 -0500

    added testing for random number generator

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -84,3 +84,33 @@ pub fn random_num(min: usize, max: usize) -> usize {
 
     result
 }
+
+mod tests {
+    #[test]
+    fn rand_nums_out_of_bounds() {
+        let loops: usize = 500;
+        let mut numbers: Vec<usize> = vec![];
+
+        fn looper(numbers: &mut Vec<usize>, loops: usize) {
+            let min: usize = 0;
+            let max: usize = loops;
+
+            let rand = super::random_num(min, max);
+
+            if rand < min || rand > max {
+                panic!("The random number generator went out of bounds.")
+            }
+
+            numbers.push(rand);
+
+            if loops == 0 {
+                return;
+            }
+
+            looper(numbers, loops - 1)
+        }
+
+        looper(&mut numbers, loops);
+        println!("{:?}", numbers);
+    }
+}
